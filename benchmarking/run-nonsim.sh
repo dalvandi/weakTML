5347e6429006fca8817fb1b7a51672da9bf7bcc6
Sadegh Dalvandi <dalvandi@gmail.com>
2021-10-30 10:24:46 +0000

    benchmarking updated

--- benchmarking/run-nonsim.sh
+++ benchmarking/run-nonsim.sh
@@ -7,13 +7,13 @@ echo "Running genome"
 bash benchmark.sh $stamp genome "-g16384 -s64 -n16777216" $n 5 > output/$name-genome.txt
 echo "Running intruder"
 bash benchmark.sh $stamp intruder "-a10 -l128 -n262144 -s1" $n 5 > output/$name-intruder.txt
-#echo "Running labyrinth"
-#bash benchmark.sh $stamp labyrinth "-i $stamp/labyrinth/inputs/random-x512-y512-z7-n512.txt" $n 3 > output/$name-labyrinth.txt
+echo "Running labyrinth"
+bash benchmark.sh $stamp labyrinth "-i $stamp/labyrinth/inputs/random-x512-y512-z7-n512.txt" $n 3 > output/$name-labyrinth.txt
 echo "Running ssca2"
 bash benchmark.sh $stamp ssca2 "-s20 -i1.0 -u1.0 -l3 -p3" $n 32 > output/$name-ssca2.txt
-#echo "Running vacation (low)"
-#bash benchmark.sh $stamp vacation "-n4 -q60 -u90 -r16384 -t4096" $n 11 > output/$name-vacation-low.txt
-#echo "Running vacation (high)"
-#bash benchmark.sh $stamp vacation "-n4 -q60 -u90 -r16384 -t4096" $n 11 > output/$name-vacation-high.txt
+echo "Running vacation (low)"
+bash benchmark.sh $stamp vacation "-n4 -q60 -u90 -r16384 -t4096" $n 11 > output/$name-vacation-low.txt
+echo "Running vacation (high)"
+bash benchmark.sh $stamp vacation "-n4 -q60 -u90 -r16384 -t4096" $n 11 > output/$name-vacation-high.txt
 echo "Running yada"
 bash benchmark.sh $stamp yada "-a15 -i $stamp/yada/inputs/ttimeu1000000.2" $n 5 > output/$name-yada.txt
